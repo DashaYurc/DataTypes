58e079128e640386e5fc06ece3c5a0a072ee7dbc
Dasha Yurchenko <dasha.yurchenko@inbox.lv>
2021-06-18 20:56:13 +0300

    First steps

---
+++ src/lv/sda/Main.java
@@ -0,0 +1,31 @@
+package lv.sda;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int firstNumber = 5;
+	    int secondNumber; //declaration
+	    secondNumber = 6; //initializing
+
+        int sum = firstNumber + secondNumber;
+        System.out.println(sum);
+
+        float firstFloat = 0.5f;
+        float secondFloat = 2.3f;
+
+        float floatSum = firstFloat + secondFloat;
+        System.out.println(floatSum);
+
+        double firstDouble = 0.7;
+        double secondDouble = 2.4;
+        double doubleSum = firstDouble + secondDouble;
+        System.out.println(doubleSum);
+
+        String firstString = "Chips";
+        String secondString = "Chicken";
+
+        System.out.println(firstString);
+        
+
+    }
+}
